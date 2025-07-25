fc9e2a21d053be1327abee6f7b6a7b52fdecd14e
hubmainv4 <ezcrypxdd@gmail.com>
2025-07-25 18:49:28 +0300

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,26 @@
+local function load()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hubmainv4/v/refs/heads/main/neo", true))()
+end
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hubmainv4/v/refs/heads/main/acb"))()
+wait(1)
+
+local successacb, result = pcall(function()
+    return hookfunction(nil, function() end)
+end)
+
+-- Eğer hata mesajı belirttiğin şekilde değilse, uyarı gönder
+if not successacb or not tostring(result):find("function expected on argument 1, got nil on hookfunction") then
+    -- Roblox notification mesajı
+    local StarterGui = game:GetService("StarterGui")
+
+    StarterGui:SetCore("SendNotification", {
+        Title = "AntiCheat",
+        Text = "Anticheat not loaded, please restart the script",
+        Duration = 5,
+    })
+
+  load()
+end
