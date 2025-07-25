13122c6e590e0fadda7d43193d67a3853bfbac4a
hubmainv4 <ezcrypxdd@gmail.com>
2025-07-25 18:50:31 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,6 @@
-local function load()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/hubmainv4/v/refs/heads/main/neo", true))()
-end
+
+
+
 
 
 
@@ -21,6 +21,8 @@ if not successacb or not tostring(result):find("function expected on argument 1,
         Text = "Anticheat not loaded, please restart the script",
         Duration = 5,
     })
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/hubmainv4/v/refs/heads/main/neo", true))()
+end
 
-  load()
 end
